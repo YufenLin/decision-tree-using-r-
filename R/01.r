f0b59fecf1a6960b9e8e6f2dbdbbffb708889fcd
YufenLin <yufenlin33@gmail.com>
2019-12-19 15:04:26 -0800

    plot pairs

--- R/01.r
+++ R/01.r
@@ -6,14 +6,19 @@
 # remove all objects in the Global Environment -----
 rm(list = ls())
 
+# R的Caret
 # install necessary packages ------
 # data scource: https://www.rdocumentation.org/packages/titanic/versions/0.1.0 
 install.packages("titanic")
 install.packages("tidyverse")
+install.packages("psych")
+install.packages("PerformanceAnalytics")
+
 # load necessary packages ------
 library(titanic)
 library(tidyverse)
-library(stringi)
+library(psych)
+# library(stringi)
 # load necessary data ---------
 titanic <-
   titanic_train
@@ -24,6 +29,7 @@ titanic %>% dim() # 891  12
 
 titanic %>%
   View(title = "Titanic")
+
 head(titanic)
 
 titanic %>% glimpse()
@@ -47,36 +53,80 @@ names(titanic) # check out all the variables in the data set
 
 summary(titanic)
 
-summary(titanic$Age) # There is 177 NA's in Age
+
 titanic$Embarked[grepl("^\\s*$", titanic$Embarked)] # There is two "" in Embarked
 # Drop Embark = "" ------
 titanic <- droplevels(titanic[!grepl("^\\s*$", titanic$Embarked),,drop=FALSE] )
 # fill NA's in Age using mean of Age -----
+summary(titanic$Age) # There is 177 NA's in Age
 titanic$Age[is.na(titanic$Age)] <- 
   round(mean(titanic$Age, na.rm = TRUE))
 
-titanic$Survived = if_else(titanic$Survived == 0
+titanic$Sex.num = if_else(titanic$Sex == "male"
+                               , 1
+                               , 0)
+
+titanic$Survived.str = if_else(titanic$Survived == 0
                            , "Did not survive"
                            , "Survived")
 
 # catogorical variables ------
 titanic$Survived = as.factor(titanic$Survived)
+titanic$Survived.str = as.factor(titanic$Survived.str)
 titanic$Pclass = as.factor(titanic$Pclass)
 titanic$Sex = as.factor(titanic$Sex)
 titanic$Embarked = as.factor(titanic$Embarked)
 
 
+# n_data = dim(titanic)[1]
+
 summary(titanic)
+df <- 
+  titanic %>% 
+  select(Survived, Pclass, Sex.num, Age, SibSp, Parch, Fare)
+
+#balance?
+df %>%
+  count(Survived)
+# plot pairs of df ------
+pairs.panels(df[,],
+             ellipses=FALSE,
+             pch = 19, 
+             hist.col="blue")
 
 # Analysis & Visualisations ------
+# plot survival Rate by Gender ------
+ggplot(titanic, 
+       aes(x=Sex, fill=Survived.str)) + 
+  geom_bar(position="fill") +
+  labs(title = "Survival Rate by Gender"
+       , caption = "Source: Titanic Passenger Survival Data Set v 0.1.0 by RDocumentation") +
+  xlab("Gender") +
+  ylab("Frequency") +
+  theme_minimal() +
+  scale_fill_manual(values = c("red", "green"))
+
+# plot survival number by Gender -----
+ggplot(titanic,
+       aes(x=Pclass,fill=Survived.str)) +
+  geom_bar(position="fill") +
+  xlab("Ticket Class") +
+  ylab("Frequency") +
+  theme_minimal() +
+  scale_fill_manual(values = c("red", "green"))
+
+
+
 ggplot(titanic, 
        aes(x=Sex, fill=Survived)) + 
-  geom_bar() +
+  geom_bar(position = "fill") +
   labs(title = "Survival Rate by Gender"
        , caption = "Source: Titanic Passenger Survival Data Set v 0.1.0 by RDocumentation") +
   xlab("Gender") +
   ylab("Number of Passenger") +
-  theme_minimal()
+  theme_minimal() +
+  scale_fill_manual(values = c("red", "green"))
+
 
 
 
